3ae4d703bdebacbcf107c58204490670238c4c32
Puluomiyuhun <53259584+Puluomiyuhun@users.noreply.github.com>
2023-05-09 23:40:26 +0800

    Add files via upload

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,15 +1,28 @@
-#include <stdio.h>
 #include <TibbieEngine.h>
+
+class TestLayer : public Tibbie::Layer{
+public:
+	TestLayer(): Layer("Test")
+	{
+
+	}
+	void OnUpdate() override {
+		//TB_CLIENT_INFO("Updating");
+	}
+	void OnEvent(Tibbie::Event &e) override {
+		//TB_CLIENT_TRACE(e.ToString());
+	}
+};
 
 class Sandbox : public Tibbie::Application {
 public:
 	Sandbox() {
-
+		PushLayer(new TestLayer());
+		PushOverlay(new Tibbie::ImGuiLayer());
 	}
 	~Sandbox() {
 
 	}
-
 };
 
 Tibbie::Application* Tibbie::CreateApplication() {
